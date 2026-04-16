493a4acec4780dcd77e987f49e4dfbea358eb2c4
Hussain Abbas Ahmed Alhakeem <halhakeem2@gmail.com>
2026-04-16 15:17:16 +0300

    made some changes

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.get("/", (req, res) => {
 
 // Server Start
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}...`)
 })
