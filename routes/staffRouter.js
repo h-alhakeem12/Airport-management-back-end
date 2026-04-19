6fbf91f5908a6296a2902c6c0f5c975f1b684215
Hussain Abbas Ahmed Alhakeem <halhakeem2@gmail.com>
2026-04-19 14:15:40 +0300

    fix Ali errors

--- routes/staffRouter.js
+++ routes/staffRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const staffController = require("../controllers/staffController")
 
 router.get("/", staffController.getStaff)
-router.get("/", staffController.getAdmin)
+router.get("/admin", staffController.getAdmin)
 router.get("/:id", staffController.getStaffById)
 router.put("/:id", staffController.updateStaff)
 router.delete("/:id", staffController.deleteStaff)
