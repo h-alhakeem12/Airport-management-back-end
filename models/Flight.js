cb0dd2ef59e6e1f4fe0a19ecd2de01b27df57887
Hussain Abbas Ahmed Alhakeem <halhakeem2@gmail.com>
2026-04-20 18:46:44 +0300

    fix errors and change things

--- models/Flight.js
+++ models/Flight.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose")
 
-const flightSchema = new mongoose.Schema({
-  flightNumber: { type: String, required: true },
-  destination: { type: String, required: true },
-  departureTime: { type: Date, required: true },
-  status: { type: String, required: true },
-  pilot: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
+const flightSchema = new mongoose.Schema(
+  {
+    flightNumber: { type: String, required: true },
+    destination: { type: String, required: true },
+    departureTime: { type: Date, required: true },
+    status: { type: String, required: true },
+    pilot: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    terminal: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Terminal",
+      required: true,
+    },
   },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-  terminal: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Terminal",
-    required: true
-  }
-}, { timestamps: true })
+  { timestamps: true }
+)
 
 module.exports = mongoose.model("Flight", flightSchema)
